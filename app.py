82a047976f29418ad2b556f388b4f4100854d0e5
Shubham Tejani <64226852+shubham404-error@users.noreply.github.com>
2024-04-11 01:20:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pickle
 from sklearn.ensemble import GradientBoostingRegressor
 # Load the model
-model= pickle.load(open("gradient_boosting_regressor_model.pkl", 'rb'))
+model= pickle.load(open("linear_reg.pkl", 'rb'))
 
 # Load the data for dropdown options
 data = pd.read_csv('./clean_data.csv')
